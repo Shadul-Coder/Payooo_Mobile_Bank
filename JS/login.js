fab44940be5b82991842d9db19cb80cd600af9c8
Shadul-Coder <codershadul@gmail.com>
2025-08-23 09:03:13 +0600

    Completed Log In Page

---
+++ JS/login.js
@@ -0,0 +1,27 @@
+document
+  .querySelector(`#login-button`)
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const invalid = document.querySelector(`p`);
+    if (invalid) {
+      document.querySelector(`form`).removeChild(invalid);
+    }
+    const number = `01990199709`;
+    const pin = `2025`;
+    let inputNumber = document.querySelector(`#Number`).value;
+    let inputPin = document.querySelector(`#Pin`).value;
+    if (inputNumber === number && inputPin === pin) {
+      window.location.href = `home.html`;
+    } else {
+      let p = document.createElement(`p`);
+      if (inputNumber !== number) {
+        p.innerText = `Invalid Number`;
+      } else {
+        p.innerText = `Invalid PIN`;
+      }
+      p.classList.add("text-[15px]", "text-red-500", "text-center", "mt-3");
+      document.querySelector(`form`).appendChild(p);
+      document.querySelector(`#Number`).value = ``;
+      document.querySelector(`#Pin`).value = ``;
+    }
+  });
